a8a3f95eca366666360996d506dff6c74747424d
Josh Viktorov <joshviktorov27@gmail.com>
2021-01-29 12:20:51 -0500

    generate DP table

--- main.py
+++ main.py
@@ -1,1 +1,35 @@
-print('Hello World!')
+# Question: Can't we just compute W? Am I correct as to what it represents
+# What exactly is J? Why does it go  to n-1?
+import numpy as np
+# Generates the dynamic programming table required in order to 
+# compute the Shapley value in weighted voting games.
+
+# The returned table is of dimensions n-1 x w(N) x n-1
+# where the X[j][w][s] entry has the number of sets of size s 
+# contained in {0,...,j} and whose weight is w.
+
+# n: Int - Number of players 
+# weights: [Int] - Weights associated with each player
+# W: Int - w(N)
+def create_DP_table(n, weights,W):
+
+    # Create table with all zeroes
+    table = np.zeros((n-1,W+1,n))
+
+    for j in range(n-1):
+        table[j][0][0] = 1
+
+    table[0][weights[0]][1] = 1
+
+    for j in range(1, n-1):
+        for w in range(W+1):
+            for s in range(1,n):
+                if (w >= weights[j]):
+                    table[j][w][s] = table[j-1][w][s] + table[j-1][w-weights[j]][s-1]
+                else:
+                    table[j][w][s] = table[j-1][w][s]
+
+    return table
+
+test = create_DP_table(3, [1,2,3], 6)
+print(test)
