37eb9bb05cc56a663a15e6faa8f1dc7b4595495c
Josh Viktorov <joshviktorov27@gmail.com>
2021-05-04 21:10:27 -0400

    coalition struct takes n instead of player list

--- coalition_structures.py
+++ coalition_structures.py
@@ -2,7 +2,8 @@ from more_itertools import set_partitions
 from wvg import wvg
 from ttg import ttg
 
-def find_best_initial_coalition_struct(players, fun):
+def find_best_initial_coalition_struct(n, fun):
+    players = list(range(n))
     second_two_levels = list(set_partitions(players, 2))
     second_two_levels.append([players])
     max_val = 0
@@ -23,5 +24,5 @@ def find_best_initial_coalition_struct(players, fun):
 
 test_ttg = ttg([3,4,5], [(7,500), (9,750), (11,1000)])
 
-ans = find_best_initial_coalition_struct([0,1,2], test_ttg.v)
+ans = find_best_initial_coalition_struct(test_ttg.get_num_players(), test_ttg.v)
 print(ans)
