7a4e4536540de45db66864375a571727bc441da9
Josh Viktorov <joshviktorov27@gmail.com>
2021-05-04 21:07:51 -0400

    initial coalition struct commit

---
+++ coalition_structures.py
@@ -0,0 +1,27 @@
+from more_itertools import set_partitions 
+from wvg import wvg
+from ttg import ttg
+
+def find_best_initial_coalition_struct(players, fun):
+    second_two_levels = list(set_partitions(players, 2))
+    second_two_levels.append([players])
+    max_val = 0
+    max_coalition = []
+
+    for partition in second_two_levels:
+        partition_val = 0
+        for player_set in partition:
+            partition_val += fun(player_set)
+        
+
+        if partition_val > max_val:
+            max_val = partition_val
+            max_coalition = partition
+
+    return (max_val, max_coalition)
+
+
+test_ttg = ttg([3,4,5], [(7,500), (9,750), (11,1000)])
+
+ans = find_best_initial_coalition_struct([0,1,2], test_ttg.v)
+print(ans)
